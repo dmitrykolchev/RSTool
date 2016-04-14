93b437006c60eb8c7bc465120ca3dc81c41a656d
dmitrykolchev <dmitrykolchev@msn.com>
2016-04-14 12:22:23 +0300

    Add project files.

---
+++ RSServices/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSServices.RS2010;
+
+
+namespace RSServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReportingService2010SoapClient client = new ReportingService2010SoapClient();
+            client.Open();
+            
+        }
+    }
+}
